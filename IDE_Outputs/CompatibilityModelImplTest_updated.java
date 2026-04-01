70f85a209cb71c553da4904b72aa6c110bc24512
sksreddy252-gif <sksreddy252@gmail.com>
2026-04-01 18:45:20 +0530

    Add CompatibilityModelImplTest_updated.java to IDE_Outputs folder

---
+++ IDE_Outputs/CompatibilityModelImplTest_updated.java
@@ -0,0 +1,23 @@
+package com.tracfonecore.core.models.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import io.wcm.testing.mock.aem.mock.ResourceResolverType;
+
+import com.tracfonecore.core.beans.CompatibilityCardBean;
+import com.tracfonecore.core.beans.CompatibilityButtonBean;
+
+@ExtendWith(AemContextExtension.class)
+public class CompatibilityModelImplTest_updated {
+
+    private final AemContext context = new AemContext(ResourceResolverType.JCR_MOCK);
+    private CompatibilityModelImpl model;
+    private static final String TEST_RESOURCE_PA
